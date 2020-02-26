1f7626f70318cc80056a95c4461b057c488a9889
Matteo Sette <msette8@gmail.com>
2020-02-26 22:02:58 +0100

    hard coded api persons

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
 
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-123456",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-23455",
+        id: 1
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-992992",
+        id: 1
+    }
+];
+
 app.get("/", (req, res) => {
     res.send("<h1>Hello world</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons);
 });
 
 const PORT = 3001;
