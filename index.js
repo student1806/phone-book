0ae1605f7e85547a82dd2781bf3888280a2f77df
Matteo Sette <msette8@gmail.com>
2020-02-27 10:23:31 +0100

    3.6 get, post & delete item

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 let persons = [
     {
@@ -15,12 +18,12 @@ let persons = [
     {
         name: "Dan Abramov",
         number: "12-43-23455",
-        id: 1
+        id: 3
     },
     {
         name: "Mary Poppendieck",
         number: "39-23-992992",
-        id: 1
+        id: 4
     }
 ];
 
@@ -30,6 +33,57 @@ app.get("/", (req, res) => {
 
 app.get("/api/persons", (req, res) => {
     res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+    const dataSize = persons.length;
+
+    res.send(`
+    <p>The phonebook has info for ${dataSize} people</p>
+    <p>${new Date()}</p>
+
+    `);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.filter(p => p.id === id);
+
+    res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.param.id);
+    persons = persons.filter(p => p.id !== id);
+
+    res.status(204).end();
+});
+
+const genId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0;
+    return maxId + 1;
+};
+
+app.post("/api/persons", (req, res) => {
+    const body = req.body;
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: "content missing"
+        });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: genId()
+    };
+
+    persons = persons.concat(person);
+
+    console.log(person);
+
+    res.json(person);
 });
 
 const PORT = 3001;
